b18c74a0c9c7953e5229567a9f7a8a2972d531f1
ken <ken@ken-no-macbook-air.local>
2018-03-31 12:53:21 +0900

    Initial Commit

---
+++ meetpie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meetpie
+//
+//  Created by ken on 2018/03/31.
+//  Copyright © 2018年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ meetpie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  meetpie
+//
+//  Created by ken on 2018/03/31.
+//  Copyright © 2018年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
